4a5e06cc66f2bb14c5f9a925fe44f0b800b1dbd8
GonHome <shenl@zhili-tech.com>
2019-11-22 18:13:10 +0800

    组合标签的列表

--- src/components/operation/body/right/board/NavTabs.tsx
+++ src/components/operation/body/right/board/NavTabs.tsx
@@ -1,5 +1,6 @@
 import { observer } from "mobx-react";
 import React from "react";
+import { Icon } from "office-ui-fabric-react";
 import { Operation } from "store";
 import NavTab from './NavTab';
 
@@ -13,11 +14,18 @@ export default class NavTabs extends  React.Component<IProps> {
   render() {
     const { width, operation } = this.props;
     const { graphIds } = operation;
+    console.log(width, graphIds);
     return (
-      <div className="nav-tabs">
-        {graphIds.map((id: string) => {
-          return <NavTab key={id} operation={operation} id={id}/>
-        })}
+      <div>
+        <div className="nav-tabs">
+          <Icon iconName="CaretLeft8" className="left-icon"/>
+          <div className="nav-content">
+            {graphIds.map((id: string) => {
+              return <NavTab key={id} operation={operation} id={id}/>
+            })}
+          </div>
+          <Icon iconName="CaretRight8" className="right-icon"/>
+        </div>
       </div>
     );
   }
